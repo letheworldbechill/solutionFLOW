e6b70192ea0b0be4ecfa2b73b1e4a8ce7645c529
Severin Leander <gasserseverin1@gmail.com>
2025-11-18 06:58:13 +0100

    Implement service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = "solution-flow-v1";
+
+const ASSETS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(req).then(cached => {
+      if (cached) return cached;
+      return fetch(req).then(res => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(req, resClone).catch(() => {});
+        });
+        return res;
+      });
+    })
+  );
+});
